7c31cc33c97f0fdde45cbb5e37194443bfc7b720
fsuleman2 <sydsulmn7@gmail.com>
2021-08-27 21:33:16 +0530

    register handshake done by suleman register module completed

--- examfront/src/app/app.module.ts
+++ examfront/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,6 +33,8 @@ import { HomeComponent } from './pages/home/home.component';
     MatFormFieldModule,
     FormsModule,
     MatCardModule,
+    HttpClientModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
