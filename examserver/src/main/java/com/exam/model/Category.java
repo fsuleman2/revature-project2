3da9f29c5d5298765481336355640306b4e4e7ce
fsuleman2 <sydsulmn7@gmail.com>
2021-08-30 19:08:05 +0530

    added Category,quiz and question table

---
+++ examserver/src/main/java/com/exam/model/Category.java
@@ -0,0 +1,42 @@
+package com.exam.model;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="category")
+public class Category {
+	@Id
+	@GeneratedValue
+	private Long cid;
+	private String title;
+	@Column(length = 5000)
+	private String description;
+	
+	//one category can have many quizes
+	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JsonIgnore
+	private Set<Quiz> quizzes = new LinkedHashSet<>();
+	
+	public Category(String title, String description) {
+		this.title=title;
+		this.description = description;
+	}
+}
---
+++ examserver/src/main/java/com/exam/model/Quiz.java
@@ -0,0 +1,45 @@
+package com.exam.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Quiz {
+	@Id
+	@GeneratedValue
+	private Long qId;
+	private String title;
+	@Column(length = 5000)
+	private String description;
+	private String maxMarks;
+	private String numberOfQuestions;
+	private boolean active = false;
+	
+	//for category and quiz
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Category category;
+	
+	//for question n quiz
+	@OneToMany(mappedBy = "quiz",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@JsonIgnore
+	private Set<Question> questions = new HashSet<>();
+
+	
+}
